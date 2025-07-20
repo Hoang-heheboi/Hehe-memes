2e3687e1d0875078419324ccbb4b9446652fc71a
Hoang-heheboi <ngododo123@gmail.com>
2025-07-20 12:14:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+window.addEventListener('message', (event) => {
+    if (event.origin !== 'https://hoang-heheboi.github.io/sound/') return;
+    if (event.data === 'triggerAction') {
+   
+    }
+});
